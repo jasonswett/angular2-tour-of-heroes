07ced55618d8d5873863be66ca5e71432dcb053a
Jason Swett <jason@benfranklinlabs.com>
2016-04-11 10:16:37 -0400

    Add list of heroes.

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,14 @@ export class Hero {
   selector: 'my-app',
   template:`
     <h1>{{title}}</h1>
+
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div>
@@ -24,4 +32,18 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
+  public heroes = HEROES;
 }
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
