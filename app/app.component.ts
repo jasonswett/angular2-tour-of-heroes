6e69a1abf714b11fe581c2f9e5aa4c4d4b3dc8b2
Jason Swett <jason@benfranklinlabs.com>
2016-04-11 11:54:15 -0400

    Refactoring.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
+import {OnInit} from 'angular2/core';
 
 @Component({
   selector: 'my-app',
@@ -67,25 +69,23 @@ import {HeroDetailComponent} from './hero-detail.component';
       border-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   selectedHero: Hero;
   onSelect(hero: Hero) { this.selectedHero = hero; }
-  public heroes = HEROES;
-}
+  heroes: Hero[];
 
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
+  constructor(private _heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes() {
+    this.heroes = this._heroService.getHeroes();
+  }
+}
---
+++ app/hero.service.ts
@@ -0,0 +1,9 @@
+import {HEROES} from './mock-heroes';
+import {Injectable} from 'angular2/core';
+
+@Injectable()
+export class HeroService {
+  getHeroes() {
+    return HEROES;
+  }
+}
