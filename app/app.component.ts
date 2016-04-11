c645a0d04a32f364c7f9e4d7442bd15c04f793be
Jason Swett <jason@benfranklinlabs.com>
2016-04-11 10:04:44 -0400

    Add hero details.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,24 @@
 import {Component} from 'angular2/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+    `
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
